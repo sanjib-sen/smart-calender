b54dbe8f839400a639a78f877f5113b7c6d3b8f4
Sanjib Kumar Sen <sksenonline@gmail.com>
2022-01-17 06:01:07 +0600

    added more features

--- Smart_Calendar/api/gcal.py
+++ Smart_Calendar/api/gcal.py
@@ -12,8 +12,14 @@ from googleapiclient.errors import HttpError
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
+def dayDiff(days):
 
-def main():
+    print(datetime.datetime.utcnow().isoformat()[:9] + str(
+        int(datetime.datetime.utcnow().isoformat()[9:10]) + days) + datetime.datetime.utcnow().isoformat()[10:])
+    return (datetime.datetime.utcnow().isoformat()[:9] + str(
+        int(datetime.datetime.utcnow().isoformat()[9:10]) + days) + datetime.datetime.utcnow().isoformat()[10:])
+
+def gcal(onlyTime, days):
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
     """
@@ -21,18 +27,18 @@ def main():
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('creds/token.json'):
-        creds = Credentials.from_authorized_user_file('creds/token.json', SCOPES)
+    if os.path.exists('api/creds/token.json'):
+        creds = Credentials.from_authorized_user_file('api/creds/token.json', SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'creds/credentials.json', SCOPES)
+                'api/creds/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('creds/token.json', 'w') as token:
+        with open('api/creds/token.json', 'w') as token:
             token.write(creds.to_json())
 
     try:
@@ -40,7 +46,7 @@ def main():
         # Call the Calendar API
         now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
         print('Getting the upcoming 10 events')
-        events_result = service.events().list(calendarId='primary', timeMin=now,
+        events_result = service.events().list(calendarId='primary', timeMin=now, timeMax=dayDiff(2) + 'Z',
                                               maxResults=10, singleEvents=True,
                                               orderBy='startTime').execute()
         events = events_result.get('items', [])
@@ -49,13 +55,20 @@ def main():
             return
 
         # Prints the start and name of the next 10 events
+        lst = []
         for event in events:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            print(start, event['summary'])
+            if onlyTime == False:    start = event['start'].get('dateTime', event['start'].get('date'))
+            else: start = event['start'].get('dateTime', event['start'].get('date'))[11:16]
+            # print(event)
+            templs = [start, event['summary']]
+            lst.append(templs)
+            # print(start, event['summary'])
+
+            print(templs)
 
     except HttpError as error:
         print('An error occurred: %s' % error)
-
+    return lst
 
 if __name__ == '__main__':
-    main()
+    gcal(True, 1)
